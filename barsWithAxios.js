c742fe94f8b2a326c898475b5f0f46c735b40d70
taimoor sheikh <taimoor.sheikh13@gmail.com>
2020-03-02 12:41:59 +0500

    first commit

--- barsWithAxios.js
+++ barsWithAxios.js
@@ -1,18 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
+import axios from 'axios';
 import Progress from "react-progressbar";
 import { Line } from "rc-progress";
 import "rc-progress/assets/index.css";
 import "./App.css";
 
 function App() {
-  const buttons = [22, 14, -24, -18];
-  const bars = [12, 11, 60, 11];
-  const [myBar, setMyBar] = useState([
-    { bar: 12, color: "blue" },
-    { bar: 11, color: "blue" },
-    { bar: 60, color: "blue" },
-    { bar: 11, color: "blue" }
-  ]);
+  // const buttons = [22, 14, -24, -18];
+  // const bars = [12, 11, 60, 11];
+  const [myBar, setMyBar] = useState([]);
+  const [bars, setMyBars] = useState([]);
+  const [buttons, setMyButtons] = useState([]);
   const [percent, setPercent] = useState(10);
 
   const [color, setColor] = useState("blue");
@@ -20,21 +18,40 @@ function App() {
   const barRef = useRef(null);
   // const [value, setValue] = useState(10);
 
-  const barDependency = myBar[progressBar].bar;
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('http://pb-api.herokuapp.com/bars');
+      console.log(result);
+      let newArr = [...myBar];
+      result.data.bars.map((bar,index) => {
+        return (
+          newArr[index] = {bar:bar, color:'blue'}
+        )
+      })
+      console.log(newArr);
+      setMyBar(newArr);
+      setMyBars(...bars,result.data.bars);
+      setMyButtons(...buttons,result.data.buttons);
+    }
+    fetchData();
+  }, [])
+  
+  // const barDependency = myBar[progressBar].bar;
 
   useEffect(() => {
-    if (myBar[progressBar].bar > 100) {
+    if (myBar[progressBar] && myBar[progressBar].bar > 100) {
       // debugger
       let newArr = [...myBar];
       newArr[progressBar].color = "red";
       setMyBar(newArr);
     } else {
       let newArr = [...myBar];
-      newArr[progressBar].color = "blue";
+      newArr[progressBar] ? newArr[progressBar].color = "blue" : console.log(null) 
       setMyBar(newArr);
       // setColor("blue");
     }
-  }, [barDependency]);
+  }, [myBar[progressBar] && myBar[progressBar].bar]);
 
   const containerStyle = {
     width: "250px"
@@ -91,13 +108,13 @@ function App() {
       <div style={containerStyle}>
         {bars.map((bar, index) => {
           return (
-            <div>
-              <h3>Line Progress {myBar[index].bar}%</h3>
+            <div key={index}>
+              <h3>Line Progress {myBar[index] && myBar[index].bar}%</h3>
               <Line
                 key={index}
-                percent={myBar[index].bar}
+                percent={myBar[index] && myBar[index].bar}
                 strokeWidth="4"
-                strokeColor={myBar[index].color}
+                strokeColor={myBar[index] && myBar[index].color}
               />
             </div>
           );
@@ -139,3 +156,4 @@ function App() {
 }
 
 export default App;
+
